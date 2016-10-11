c52db2b814787aba96747d3a23c3b815075821cf
syugoing <shugo.sawada.18@gmail.com>
2016-10-11 03:51:15 +0000

    fog

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,38 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+x=1
+100.times do |n|
+  email = Faker::Internet.email
+  password = "password"
+  name = Faker::Pokemon.name
+  uid = x
+  x = x+1
+  provider = "seed"
+  User.create!(email: email,
+               password: password,
+               password_confirmation: password,
+               name: name,
+               uid: uid,
+               provider: provider,
+               )
+end
+
+a = 1
+99.times do |n|
+  content = Faker::Food.ingredient
+  user_id = a
+  a == a+1
+  User.create!(content: content,
+                user_id: user_id,
+                )
+end
+
+b = 1
+99.times do |n|
+  content = Faker::Food.ingredient
+  user_id = b
+  topic_id = b
+  b == b+1
+  User.create!(content: content,
+                user_id: user_id,
+                topic_id: topic_id,
+                )
+end
