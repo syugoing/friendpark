29ea6f0d3f09111ac75e1358743594423ef0e2b4
syugoing <shugo.sawada.18@gmail.com>
2016-10-12 13:23:12 +0000

    AWS fix

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,2 +1,2 @@
-server 'EC2のIPアドレス', user: 'app', roles: %w{app db web}
-set :ssh_options, keys: '鍵へのpath'
+server '52.43.207.172', user: 'app', roles: %w{app db web}
+set :ssh_options, keys: '~/.ssh/id_rsa'
