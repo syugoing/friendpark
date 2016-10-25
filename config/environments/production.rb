0f1356b2902d9e99f0086d332684e6528b7fa652
syugoing <shugo.sawada.18@gmail.com>
2016-10-25 08:29:17 +0000

    fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -12,18 +12,34 @@ Rails.application.configure do
   config.active_support.deprecation = :notify
   config.log_formatter = ::Logger::Formatter.new
   config.active_record.dump_schema_after_migration = false
+# prev
+  # config.action_mailer.default_url_options = { host: '52.37.235.201' }
+  # ActionMailer::Base.delivery_method = :smtp
+  # ActionMailer::Base.smtp_settings =
+  # {
+  #  user_name: ENV['SENDGRID_USERNAME'],
+  #  password: ENV['SENDGRID_PASSWORD'],
+  #  domain: "heroku.com",
+  #  address: "smtp.sendgrid.net",
+  #  port: 587,
+  #  authentication: :plain,
+  #  enable_starttls_auto: true
+  # }
+
 
   config.action_mailer.default_url_options = { host: '52.37.235.201' }
-  ActionMailer::Base.delivery_method = :smtp
-  ActionMailer::Base.smtp_settings =
-  {
-   user_name: ENV['SENDGRID_USERNAME'],
-   password: ENV['SENDGRID_PASSWORD'],
-   domain: "heroku.com",
-   address: "smtp.sendgrid.net",
-   port: 587,
-   authentication: :plain,
-   enable_starttls_auto: true
-  }
+  # config.action_mailer.default_url_options = { host: '52.43.207.172' }
+    ActionMailer::Base.delivery_method = :smtp
+    ActionMailer::Base.smtp_settings =
+    {
+     user_name: ENV['SENDGRID_USERNAME'],
+     password: ENV['SENDGRID_PASSWORD'],
+     domain: "AWS",
+     address: "smtp.sendgrid.net",
+     port: 587,
+     authentication: :plain,
+     enable_starttls_auto: true
+    }
+
 
 end
